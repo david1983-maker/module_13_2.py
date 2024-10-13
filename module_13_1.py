cd652a9ee2b6e4e4432f24d7a8cdc833e55a5620
david1983-maker <davidkilidzan94127@gmail.com>
2024-10-13 22:40:13 +0300

    Update module_13_2.py

--- module_13_1.py
+++ module_13_1.py
@@ -1,24 +1,26 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-
-async def start_strongman(name, power):
-    count_balls = 1
-    time_count = 10
-    print(f'Силач {name} начал соревнования')
-    for i in range(1, 5 + 1):
-        await asyncio.sleep(time_count / power)
-        print(f'Силач {name} поднял {count_balls} шар')
-        count_balls += 1
-    print(f'Силач {name} закончил соревнования')
+api = ''
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
 
 
-async def start_tournament():
-    task1 = asyncio.create_task(start_strongman('Pasha', 3))
-    task2 = asyncio.create_task(start_strongman('Denis', 4))
-    task3 = asyncio.create_task(start_strongman('Apollon', 5))
-    await task1
-    await task2
-    await task3
+@dp.message_handler(commands=['start'])
+async def start(message):
+    print('Привет! Я бот помогающий твоему здоровью')
 
 
-asyncio.run(start_tournament())
+@dp.message_handler()
+async def all_messages(message):
+    print('Введите команду /start, чтобы начать общение')
+
+
+@dp.message_handler()
+async def all_message(message):
+    print('Мы получили сообщение')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
